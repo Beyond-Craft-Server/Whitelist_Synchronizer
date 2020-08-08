19ec25e04da876087e0ac7193f96dcb04c39a20c
WhitePaper_ <30739658+AngelicaRoot@users.noreply.github.com>
2020-08-08 22:15:23 +0800

    Add files via upload

---
+++ Whitelist_Synchronizer.py
@@ -0,0 +1,48 @@
+import time
+
+
+whitelist_path = ''
+command = '!!whitelist sync'
+
+
+def get_whitelist(server):
+    """通过指令获取白名单"""
+    server.execute('whitelist list')
+
+
+def server_output_to_list(info):
+    """将服务器返回的白名单转换为列表"""
+    full_whitelist = list(info.content.split('[')[1].split(']')[0].split(', '))
+    return full_whitelist
+
+
+def add_new_ID(whitelist):
+    """将新ID加入白名单"""
+    with open(whitelist_path) as wl:
+        whitelist_new = list(wl.read().split('\n'))
+        for ID in whitelist:
+            if ID not in whitelist_new:
+                whitelist_new.append(ID)
+            else:
+                pass
+        return whitelist_new
+
+
+def write_file(whitelist):
+    """写入白名单文件"""
+    with open(whitelist_path, 'w') as wl:
+        wl.write('')
+        for ID in whitelist:
+            wl.write('{}\n'.format(ID))
+
+
+State = False
+def on_info(server, info):
+    global State
+    if info.content == command:
+        get_whitelist(server)
+        State = True
+        time.sleep(1)
+        State = False
+    if info.content.startswith('Whitelist: [') and not info.is_player and State:
+        write_file(add_new_ID(server_output_to_list(info)))
